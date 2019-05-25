1be530b4456d3fc5ea1e939d97e7597cf61c7b07
itsu020402 <tsubaki394>
2019-05-25 21:29:00 +0900

    update 1

--- src/main/java/dev/itsu/midiplayerapi/plugin/Main.java
+++ src/main/java/dev/itsu/midiplayerapi/plugin/Main.java
@@ -1,21 +1,8 @@
 package dev.itsu.midiplayerapi.plugin;
 
-import cn.nukkit.Server;
-import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
-import cn.nukkit.event.player.PlayerInteractEvent;
 import cn.nukkit.plugin.PluginBase;
-import cn.nukkit.utils.TextFormat;
-import dev.itsu.midiplayerapi.core.MIDILoader;
 import dev.itsu.midiplayerapi.core.MIDIPlayerAPI;
-import dev.itsu.midiplayerapi.entity.MIDIPlayer;
-import dev.itsu.midiplayerapi.entity.model.instrument.Instruments;
-import dev.itsu.midiplayerapi.entity.model.instrument.NoteBlockSet;
-import dev.itsu.midiplayerapi.event.SetPauseEvent;
-import dev.itsu.midiplayerapi.event.StartPlayingEvent;
-import dev.itsu.midiplayerapi.event.StopPlayingEvent;
-import dev.itsu.midiplayerapi.event.listener.MIDIEventHandler;
-import dev.itsu.midiplayerapi.event.listener.MIDIEventListener;
 
 import java.io.File;
 
